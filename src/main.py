5bb7548075e2accbf2dc49ce07ae8ef0eaed9eff
Sloeffler123 <samloeffler123@gmail.com>
2025-03-28 07:47:40 -0500

    boiler code with testing

---
+++ src/main.py
@@ -0,0 +1,14 @@
+from textnode import TextNode
+
+
+
+
+
+
+
+def main():
+    text = TextNode(text='this is some text', text_type='link', url='https://www.boot.dev')
+    print(text.__repr__())
+main()
+
+
