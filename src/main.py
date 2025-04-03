4323a528c70f82498e9db717867acb45cfcaa46e
Sloeffler123 <samloeffler123@gmail.com>
2025-04-03 07:47:03 -0500

    copy files to other directory

--- src/main.py
+++ src/main.py
@@ -1,16 +1,33 @@
 from textnode import TextNode
-
-
-
-
-
-
+import shutil
+import os 
 
 def main():
-    text = TextNode(text='this is some text', text_type='link', url='https://www.boot.dev')
-    print(text.__repr__())
-main()
-
-
-
     
+    copy_static(source='static', destination='public')
+
+def copy_static(source, destination):
+    if source == 'static' and os.path.exists('public'):
+        shutil.rmtree(destination)
+        os.mkdir(destination)
+    elif not os.path.exists(destination):
+        os.mkdir(destination)    
+    entries_in_source_directory = os.listdir(source)
+    
+    print(entries_in_source_directory)
+    
+    for entry in entries_in_source_directory:
+        entries_in_source_directory = os.path.join(source, entry)
+        destination_directory = os.path.join(destination, entry)
+        print(f"Source: {source}, Destination: {destination}")
+        print(f"Working on entry: {entry}")
+
+        if os.path.isfile(entries_in_source_directory):
+            shutil.copy(entries_in_source_directory, destination_directory)
+        else:
+            if not os.path.exists(destination_directory):
+                os.mkdir(destination_directory)
+            
+            copy_static(entries_in_source_directory, destination_directory)
+    
+main()
