e567a91d8cc3059354c3483096fe218c8aecbfc4
Sloeffler123 <samloeffler123@gmail.com>
2025-04-04 07:08:57 -0500

    changes

--- src/main.py
+++ src/main.py
@@ -5,23 +5,25 @@ from markdown_blocks import markdown_to_html_node, extract_title
 from pathlib import Path
 import sys
 
-
-
-
-
-
-
+dir_path_static = "./static"
+dir_path_public = "./docs"
+dir_path_content = "./content"
+template_path = "./template.html"
+default_basepath = "/"
 
 
 
 def main():
-    basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
-    copy_static(source='static', destination='public')
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+
+    copy_static(source=dir_path_static, destination=dir_path_public)
     
     generate_pages_recursive(
-        dir_path_content="./content",          # Source markdown directory
-        template_path="./template.html",       # Path to your template file
-        dest_dir_path="./docs",
+        dir_path_content=dir_path_content,          # Source markdown directory
+        template_path=template_path,       # Path to your template file
+        dest_dir_path=dir_path_public,
         basepath=basepath           # Destination directory
     )
 def copy_static(source, destination):
@@ -31,21 +33,16 @@ def copy_static(source, destination):
     elif not os.path.exists(destination):
         os.mkdir(destination)    
     entries_in_source_directory = os.listdir(source)
-    
-    print(entries_in_source_directory)
-    
     for entry in entries_in_source_directory:
         entries_in_source_directory = os.path.join(source, entry)
         destination_directory = os.path.join(destination, entry)
         print(f"Source: {source}, Destination: {destination}")
         print(f"Working on entry: {entry}")
-
         if os.path.isfile(entries_in_source_directory):
             shutil.copy(entries_in_source_directory, destination_directory)
         else:
             if not os.path.exists(destination_directory):
                 os.mkdir(destination_directory)
-            
             copy_static(entries_in_source_directory, destination_directory)
 
 def generate_page(from_path, template_path, dest_path, basepath):
@@ -57,7 +54,7 @@ def generate_page(from_path, template_path, dest_path, basepath):
     template_text_to_html = markdown_to_html_node(content) # from path file
     html = template_text_to_html.to_html()
     title = extract_title(content)
-    full_html = file.replace('{{ Title }}', title).replace('{{ Content }}', html).replace('href="/', 'href="{BASEPATH}').replace('src="/', 'src="{BASEPATH}')#changing the template html file
+    full_html = file.replace('{{ Title }}', title).replace('{{ Content }}', html).replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')#changing the template html file
     dir_name = os.path.dirname(dest_path)
     if not os.path.exists(dir_name):
         os.makedirs(dir_name)
@@ -73,7 +70,4 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, bas
             generate_page(from_path, template_path, dest_path, basepath)
         else:
             generate_pages_recursive(from_path, template_path, dest_path, basepath)
-
-
-
 main()
